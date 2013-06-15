f6165b6a6da914e72583b4d0d9967a34501fd00e
fretzl <f.sondaar@kpnmail.nl>
2013-06-15 19:09:50 +0200

    New example logo and rework of the code

--- zp-branding.php
+++ zp-branding.php
@@ -17,35 +17,91 @@ $option_interface = 'zpBranding';
 zp_register_filter('admin_head', 'zpBranding::customZpLogo');
 
 class zpBranding {
+
+	function __construct() {
+		$path = SERVERPATH.'/'.USER_PLUGIN_FOLDER.'/zp-branding/zp-admin-logo';
+		$matching = safe_glob($path . ".*");
+		if (count($matching) !== 0) {
+			$path_parts = pathinfo(array_shift($matching)); 
+			$file = SERVERPATH.'/'.USER_PLUGIN_FOLDER.'/zp-branding/'.$path_parts['basename'];
+			list($width, $height) = getimagesize($file);
+			setOptionDefault('width', $width);
+			setOptionDefault('height', $height);
+			setOptionDefault('restore', 0);
+			} else { ?>
+				<div class="errorbox">
+				<?php echo gettext('No image found.'); exitZP(); ?>
+				</div>
+			<?php
+			}	
+	}
 	
 	function getOptionsSupported() {
 		return array(	gettext('Width') => array('key' => 'width', 'type' => OPTION_TYPE_TEXTBOX,
-										'desc' => gettext('The width of the image (px)')),
+										'order'=> 1,
+										'desc' => gettext('The width of the image (px). Default is the original width.')),
 						gettext('Height') => array('key' => 'height', 'type' => OPTION_TYPE_TEXTBOX,
-										'desc' => gettext('The height of the image (px)'))
+										'order'=> 2,
+										'desc' => gettext('The height of the image (px). Default is the original height.')),
+						gettext('Reset') => array('key' => 'restore', 'type' => OPTION_TYPE_CHECKBOX,
+										'order'=> 3,
+										'desc' => gettext('Reset to the original width and height.'))
 		);
 	}
 	
 	static function customZpLogo() {
 		global $_zp_current_admin_obj,$_zp_admin_tab,$_zp_admin_subtab,$_zp_gallery;
 		if (zp_loggedin(ADMIN_RIGHTS)) {
-		$new_src = WEBPATH.'/'.USER_PLUGIN_FOLDER.'/zp-branding/zp-admin-logo.png';
-		$new_title = sprintf(gettext('%1$s administration'),html_encode($_zp_gallery->getTitle()),html_encode($_zp_admin_tab));
-		$new_alt = sprintf(gettext('%1$s administration'), html_encode($_zp_gallery->getTitle()));
-			?>
-			<script type="text/javascript">
-			// <!-- <![CDATA[
-				$(document).ready(function(){
-					$('#administration img#logo')
-					.prop("src","<?php echo $new_src; ?>")
-					.prop("title","<?php echo $new_title; ?>")
-					.prop("alt","<?php echo $new_alt; ?>")
-					.css({'width':'<?php echo getOption("width"); ?>', 'height':'<?php echo getOption("height"); ?>'});
-				});
-			// ]]> -->
-			</script>
-			
-		<?php					
+		$path = SERVERPATH.'/'.USER_PLUGIN_FOLDER.'/zp-branding/zp-admin-logo';
+		$matching = safe_glob($path . ".*");
+		if (count($matching) == 1) { // check if there is more than one file with the name "zp-admin-logo".
+			$path_parts = pathinfo(array_shift($matching)); 
+			$ext = $path_parts['extension'];
+				if ($ext == 'jpg' || $ext == 'jpeg' || $ext == 'png' || $ext == 'gif') {
+					$file = WEBPATH.'/'.USER_PLUGIN_FOLDER.'/zp-branding/'.$path_parts['basename'];
+					$new_src = $file;
+					$new_title = sprintf(gettext('%1$s administration'),html_encode($_zp_gallery->getTitle()),html_encode($_zp_admin_tab));
+					$new_alt = sprintf(gettext('%1$s administration'), html_encode($_zp_gallery->getTitle()));
+					?>
+					<script type="text/javascript">
+					// <!-- <![CDATA[
+						$(document).ready(function(){
+							$('#administration img#logo')
+							.prop("src","<?php echo $new_src; ?>")
+							.prop("title","<?php echo $new_title; ?>")
+							.prop("alt","<?php echo $new_alt; ?>")
+							.css({'width':'<?php echo getOption("width"); ?>', 'height':'<?php echo getOption("height"); ?>'});
+						});
+					// ]]> -->
+					</script>
+					<?php
+					} else { ?>
+						<div class="errorbox">
+						<?php echo gettext('No valid file type found.'); ?>
+						</div>
+					<?php
+					}
+			} else { 
+				if (count($matching) > 1) { ?>
+				<div class="errorbox">
+				<?php echo gettext('ERROR: There is more than one file with the name <code>zp-admin-logo</code>'); ?>
+				</div>
+			<?php
+			}
+			}					
+		}
+	}
+	
+	function handleOptionSave() {
+		$path = SERVERPATH.'/'.USER_PLUGIN_FOLDER.'/zp-branding/zp-admin-logo';
+		$matching = safe_glob($path . ".*");
+		$path_parts = pathinfo(array_shift($matching)); 
+		$file = SERVERPATH.'/'.USER_PLUGIN_FOLDER.'/zp-branding/'.$path_parts['basename'];
+		list($width, $height) = getimagesize($file);
+		if (getOption('restore')) {
+			setOption('width', $width);
+			setOption('height', $height);
+			purgeOption('restore');
 		}
 	}
 }
